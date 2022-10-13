b33d60872d26f2be02b2e991af7d7c21b354ed98
Bogdan Kustan <bogdan.kustan@saldo.com>
2022-10-13 14:44:24 +0300

    Arguments long version

--- cleaner.sh
+++ cleaner.sh
@@ -1,48 +1,58 @@
 #!/bin/bash
 
 keep_releases=2
-releases=$(pwd)
-while getopts ":c:p:" opt; do
-  case $opt in
-    c)
-      re='^[0-9]+$'
-      if ! [[ $OPTARG =~ $re ]] ; then
-        echo "error: -c must be an integer number" >&2; exit 1
-      fi
-      keep_releases=$OPTARG
-      ;;
-    p)
-        releases=${OPTARG%/}
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
+releases_dir=$(pwd)
+
+usage()
+{
+  echo "Usage: cleaner  [ -k | --keep RELEASES ] [ -p | --path PATH ]"
+  exit 2
+}
+
+PARSED_ARGUMENTS=$(getopt -a -n cleaner.sh -o k:p: --long keep:,path: -- "$@")
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
+fi
+eval set -- "$PARSED_ARGUMENTS"
+
+while :
+do
+  case "$1" in
+    -k | --keep) keep_releases="$2"; shift 2 ;;
+    -p | --path) releases_dir="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    *) echo "Unexpected option: $1 - this should not happen."
+       usage ;;
   esac
 done
 
-if [[ -z $releases ]]; then
-    echo "Releases direcory not provided. Useage:"
-    echo "cleaner.sh -c 2 -p /path/to/releases/directory"
+releases_dir=${releases_dir%/}
+
+if [[ "$keep_releases" -le "0" ]]; then
+    echo "Releases count must be more than 0 ($keep_releases provided)"
+    usage
     exit 1;
 fi
 
-total=$(ls -l $releases | grep '^d' | wc -l)
+if [[ -z $releases_dir ]]; then
+    echo "Releases direcory not provided"
+    usage
+    exit 1;
+fi
+
+total=$(ls -l $releases_dir | grep '^d' | wc -l)
 if [[ "$total" -lt "$keep_releases" ]]; then
     exit 0
 fi
 ((total=total-keep_releases))
 i=0
-readarray -d '' entries < <(printf '%s\0' $releases/*/ | sort -zV)
+readarray -d '' entries < <(printf '%s\0' $releases_dir/*/ | sort -zV)
 for directory in "${entries[@]}"; do
   if [[ "$total" -eq "$i" ]]; then
     exit 0
   fi
-  echo $i/$total : deleteing $directory
+  echo $((i+1))/$total : deleteing $directory
   rm -rf $directory
   ((i=i+1))
 done
